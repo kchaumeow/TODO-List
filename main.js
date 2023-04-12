0dbc58d8916befeccebb4206dfd2e511df73227f
kchaumeow <atrofimova516@gmail.com>
2023-04-12 21:34:51 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const taskForm = document.getElementById("taskForm")
+const addTaskButton = document.getElementById("taskButton")
+console.log(taskForm)
+console.log(addTaskButton)
+taskForm.addEventListener('submit', function (e){
+    e.preventDefault();
+    showNotification("New task added!");
+    }
+);
+
+function showNotification(taskName) {
+    const notification = `<div class="newTaskNotification">
+        <div>${taskName}</div>
+    </div>`;
+    document.querySelector(".container-md").innerHTML += notification
+    setTimeout(() => document.querySelector('.newTaskNotification').remove(), 5000);
+  }
+
+
+
+
+  
