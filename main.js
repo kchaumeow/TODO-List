4408d36a40b4842a7dba80edd32c4641274ab8e7
kchaumeow <atrofimova516@gmail.com>
2023-04-13 18:14:42 +0300

    version without local storage

--- main.js
+++ main.js
@@ -1,17 +1,67 @@
-const taskForm = document.getElementById("taskForm")
-const addTaskButton = document.getElementById("taskButton")
-console.log(taskForm)
-console.log(addTaskButton)
-taskForm.addEventListener('submit', function (e){
+const taskForm = document.getElementById("taskForm");
+const taskFilter = document.querySelector(".taskFilter");
+const taskList = document.querySelector(".taskList");
+// document.addEventListener("DOMContentLoaded", getLocalTasks);
+// taskList.addEventListener("click", deleteCheck);
+// taskFilter.addEventListener("change", filteringTasks);
+function addNewTask(taskName) {
+    // const newTask = `<li class="taskItem">
+    //     <div class="taskText">${taskName}</div>
+    //     </li>`;
+    const newTask = document.createElement('li');
+    newTask.classList.add("taskItem");
+    newTask.innerHTML = `<div class="taskText">${taskName}</div>`;
+    const delImg = document.createElement("img");
+    delImg.src = "delete.png";
+    delImg.id = "delete";
+    newTask.appendChild(delImg);
+    taskList.appendChild(newTask);
+}
+
+taskList.addEventListener("click", function(e){
+    if (e.target.tagName === "LI" && e.target.classList.contains("checked")){
+        e.target.classList.remove("checked");
+        e.target.classList.toggle("unchecked");
+        showRedNotification(`Task </q>${e.target.querySelector(".taskText").innerHTML}</q> incomplited`);
+    }
+    else if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        showGoldNotification(`Task <q>${e.target.querySelector(".taskText").innerHTML}</q> completed!`);
+    }
+    if (e.target.tagName === "IMG"){
+        showRedNotification(`Task <q>${e.target.parentNode.querySelector(".taskText").innerHTML}</q> was deleted!`);
+        e.target.parentNode.remove();
+    }
+
+})
+
+taskForm.addEventListener('submit', function (e) {
     e.preventDefault();
     const taskName = document.querySelector("#taskName").value;
-    addNewTask(taskName);
-    showNotification("New task added!");
+    if (taskName == "") {
+        showRedNotification("Enter task name!");
     }
-);
+    else {
+        addNewTask(taskName);
+        showGreenNotification(`New task  <q>${taskName}</q> added!`);
+    }
+    document.querySelector("#taskName").value = "";
+});
 
-function showNotification(taskName) {
+
+function showGreenNotification(taskName) {
     const notification = `<div class="newTaskNotification">
+        <div>${taskName}</div>
+    </div>`;
+    document.querySelector(".notification-container").innerHTML += notification;
+    setTimeout(() => {
+        document.querySelector('.newTaskNotification').classList.add("notificationFadeOut");
+        setTimeout(() => document.querySelector('.newTaskNotification').remove(), 1000);
+    }, 2000)
+}
+
+function showRedNotification(taskName) {
+    let notification = `<div class="newTaskNotification" id="red">
         <div>${taskName}</div>
     </div>`;
     document.querySelector(".notification-container").innerHTML += notification
@@ -19,13 +69,17 @@ function showNotification(taskName) {
         document.querySelector('.newTaskNotification').classList.add("notificationFadeOut");
         setTimeout(() => document.querySelector('.newTaskNotification').remove(), 1000);
     }, 2000)
-  }
-
-function addNewTask(taskName){
-    const newTask = `<div>${taskName}</div>`;
-    document.querySelector(".tasks-container").innerHTML += newTask;
 }
 
-
+function showGoldNotification(taskName) {
+    let notification = `<div class="newTaskNotification" id="gold">
+        <div>${taskName}</div>
+    </div>`;
+    document.querySelector(".notification-container").innerHTML += notification
+    setTimeout(() => {
+        document.querySelector('.newTaskNotification').classList.add("notificationFadeOut");
+        setTimeout(() => document.querySelector('.newTaskNotification').remove(), 1000);
+    }, 2000)
+}
 
   
